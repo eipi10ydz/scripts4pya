3b91ba165118e51960596f190ad86d0daab46817
eipi10 <gdzjydz@mail.ustc.edu.cn>
2016-01-28 22:32:12 +0800

    using 2to3 convert the code and add python3forandroid apk

--- weather4a.py
+++ weather4a.py
@@ -1,17 +1,17 @@
 #-*- coding:utf-8 -*-
 
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import re
 import android
 
 class weather(object):
-	def __init__(self,province,city):
-		self.pattern = ur'<div class="cname">\s*<a target="_blank" href="/publish/forecast/A' + province + u'/' + city + ur'.html">\s*(\S*)\s*</a>\s*</div>\s*<div class="weather">\s*(\S*)\s*</div>\s*<div class="temp">\s*(\S*)\s*</div>\s*</li>' 
+	def __init__(self, province, city):
+		self.pattern = r'<div class="cname">\s*<a target="_blank" href="/publish/forecast/A' + province + '/' + city + r'.html">\s*(\S*)\s*</a>\s*</div>\s*<div class="weather">\s*(\S*)\s*</div>\s*<div class="temp">\s*(\S*)\s*</div>\s*</li>' 
 	def weather(self):
 		try:
-			res = urllib2.urlopen('http://www.nmc.cn/publish/forecast/china.html').read().decode('utf-8')	
-		except urllib2.URLError:
-			droid.notify('网络错误','无法获取天气信息')
+			res = urllib.request.urlopen('http://www.nmc.cn/publish/forecast/china.html').read().decode('utf-8')	
+		except urllib.error.URLError:
+			droid.notify('网络错误', '无法获取天气信息')
 		else:
 			match = re.findall(self.pattern,res,re.M)
 			if match:
@@ -19,11 +19,11 @@ class weather(object):
 				self.weather = match[0][1]
 				self.combine = self.temperature + '  ' + self.weather
 				self.city = match[0][0]
-				droid.notify(city,combine)
-				
+				droid.notify(self.city, self.combine)				
+
 droid = android.Android()
 
 #you can use the class weather as my codes below
 
-#hf = weather('AH','hefei')
+#hf = weather('AH', 'hefei')
 #hf.weather()
--- wltlogin4a.py
+++ wltlogin4a.py
@@ -1,48 +1,52 @@
 #-*- coding:utf-8 -*-
 
-import urllib
-import urllib2
-import cookielib
+import urllib.request, urllib.parse, urllib.error
+import urllib.request, urllib.error, urllib.parse
+import http.cookiejar
 import re
 import android
 
 droid = android.Android()
 
-cookie=cookielib.CookieJar()
-opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+cookie=http.cookiejar.CookieJar()
+opener=urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
 
 url = 'http://wlt.ustc.edu.cn/cgi-bin/ip'
-postdata = urllib.urlencode({
+postdata = urllib.parse.urlencode({
 		"cmd":"login",
 		"url":"URL",
 		"name":#your name here,
 		"password":#your password here
-	})
+	}).encode()
+
 choice = 8
-patternWwang = ur'<table border=0 width=620 cellspacing=0 bordercolor="#cccccc"><tr><td></td><td>(\S*)<font color=red>(\S*)<br>\s*</td></tr></table>'
-patternNwang = ur'<tbody><tr><td>(\S*)<br>\s*(\S*)\s*(\S*?)<br>\s*(\S*)\s(\S*)'
+patternWwang = r'<table border=0 width=620 cellspacing=0 bordercolor="#cccccc"><tr><td></td><td>(\S*)<font color=red>(\S*)<br>\s*</td></tr></table>'
+patternNwang = r'<tbody><tr><td>(\S*)<br>\s*(\S*)\s*(\S*?)<br>\s*(\S*)\s(\S*)'
 
 try:
-	req = urllib2.Request(url = url,data = postdata)
+	req = urllib.request.Request(url = url,data = postdata)
 	result = opener.open(req)
-except urllib2.URLError:
-	droid.notify(u'网络错误',u'无法连接网络通网站')
+except urllib.error.URLError:
+	droid.notify('网络错误','无法连接网络通网站')
 else:
 	try:
-		req = urllib2.Request(url = url + '?cmd=set&url=URL&type=' + str(choice))
-		result=opener.open(req).read()
-	except urllib2.URLError:
-		droid.notify(u'网络错误',u'无法登陆网络通')
+		req = urllib.request.Request(url = url + '?cmd=set&url=URL&type=' + str(choice))
+		result=opener.open(req).read().decode('gb2312')
+	except urllib.error.URLError:
+		droid.notify('网络错误','无法登陆网络通')
 	else:
 		try:
 			res = re.findall(patternWwang,result,re.M)
 			if len(res[0]) == 2:
-				droid.notify(u'注意',u'IP地址非科大IP地址,无法设置权限')
+				droid.notify('注意','IP地址非科大IP地址,无法设置权限')
 		except IndexError:
-			res = re.findall(patternNwang,result,re.M)
-			if len(res[0]) == 5:
-				droid.notify(u'提示',u'登陆成功')
-
+			try:
+				res = re.findall(patternNwang,result,re.M)
+				if len(res[0]) == 5:
+					droid.notify('提示','登陆成功')
+			except IndexError:
+				droid.notify('提示','未开通网络通')
+				
 #replace the #... with your username and password
 #if you want to change the exit, please change the value of choice
 #the value of choice equals to the number of exit minus 1
